7427e4eb0893f44d4d441e9c85bb5ae54f28329d
varun312 <varun.pr0coder@gmail.com>
2023-11-01 00:02:35 +0530

    ML summarise added

--- app/api/summarise/route.ts
+++ app/api/summarise/route.ts
@@ -1,6 +1,13 @@
 export async function POST(request: Request) {
     const { workspace } = await request.json();
-
+    console.log(workspace)
+    // let prompt = `I am trying to brainstorm for my project and I have an agenda or a goal that I want to achieve and also a list of thoughts or half ideas that are somewhat connected and may lead to a good idea. Help me to summarise all this into a short, conside form so I can be productive. My agenda is ${workspace.agenda}. Here are some ideas that are connected to this agenda`;
+    let prompt = `My agenda is ${workspace.agenda}`;
+    for (let index = 0; index < workspace.ideas.length; index++) {
+        prompt = prompt.concat(`
+        ${index})${workspace.ideas[index].name}:${workspace.ideas[index].description}`);
+    }
+    console.log(prompt);
     const response = await fetch(
         "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
         {
@@ -9,12 +16,12 @@ export async function POST(request: Request) {
             },
             method: "POST",
             body: JSON.stringify({
-                inputs: ``,
+                inputs: prompt,
             }),
         }
     );
     const result = await response.json();
     console.log(result);
 
-    return Response.json({ msg: "Hello World" });
+    return Response.json({ msg: result["summary_text"] });
 }
