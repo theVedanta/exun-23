9953edefa4c38854d970f1cc32d17bd85c0d4d54
Vedanta Somnathe <vedanta1412@gmail.com>
2023-10-31 22:58:02 +0530

    final build

--- components/IdeaInputBox.tsx
+++ components/IdeaInputBox.tsx
@@ -1,84 +1,98 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Box, Button, Flex, Heading, IconButton } from "@radix-ui/themes";
-import React, { useEffect, useRef, useState } from "react";
-import  { OutputData } from "@editorjs/editorjs";
+import { Box, Flex, Heading, IconButton } from "@radix-ui/themes";
+import React, { useRef, useState } from "react";
+import { OutputData } from "@editorjs/editorjs";
 import { doc, updateDoc } from "firebase/firestore";
 import db from "@/app/db";
-import { Cross1Icon } from "@radix-ui/react-icons";
+import { TrashIcon } from "@radix-ui/react-icons";
 const Editor = dynamic(() => import("./Editor"), {
     ssr: false,
 });
 
 interface Props {
-	idea: Idea;
-	workspace: Workspace | undefined;
+    idea: Idea;
+    workspace: Workspace | undefined;
 }
 
 const IdeaInputBox = ({ idea, workspace }: Props) => {
-	const boxRef = useRef(null);
-	const editorRef = useRef(null);
-	const [created, setCreated] = useState(false);
+    const editorRef = useRef(null);
+    const [created, setCreated] = useState(false);
 
+    const saveChanges = async () => {
+        if (editorRef.current && workspace) {
+			// @ts-ignore
+            const outData: OutputData = await editorRef.current.save();
 
-	const saveChanges = async () => {
-		if (editorRef.current && workspace) {
-			const outData: OutputData = await editorRef.current.save();
+            const updatedIdeas = workspace.ideas?.map((item: Idea) => {
+                if (item.id === idea.id) {
+                    return { ...item, notes: outData };
+                } else return { ...item };
+            });
 
-			const updatedIdeas = workspace.ideas?.map((item: Idea) => {
-				if (item.id === idea.id) {
-					return { ...item, notes: outData };
-				} else return { ...item };
-			});
+            await updateDoc(doc(db, "workspaces", workspace.id), {
+                ideas: updatedIdeas,
+            });
+        }
+    };
 
-			await updateDoc(doc(db, "workspaces", workspace.id), {
-				ideas: updatedIdeas,
-			});
-		}
-	};
+    const removeIdea = async () => {
+        if (workspace && workspace.ideas) {
+            const ideas = workspace.ideas.filter(
+                (item, i) => item.id !== idea.id
+            );
+            await updateDoc(doc(db, "workspaces", workspace.id), {
+                ideas,
+            });
+        }
+    };
 
-	const removeIdea = async () => {
-			if (workspace && workspace.ideas) {
-				const ideas = workspace.ideas.filter((item, i) => item.id!==idea.id)
-				await updateDoc(doc(db, "workspaces", workspace.id), {
-					ideas,
-				});
-			}
-	}
+    return (
+        <Box
+            style={{
+                position: "absolute",
+                width: "400px",
+                maxHeight: "300px",
+                background: "#fff",
+                border: "2px solid #eaeefe",
+                top: "60%",
+                left: "60%",
+                padding: "28px",
+                borderRadius: "10px",
+                cursor: "text",
+                overflow: "scroll",
+                textAlign: "left",
+                resize: "both",
+            }}
+        >
+            <Flex justify="between">
+                <Heading style={{ fontSize: "19px", fontWeight: "600" }}>
+                    {idea.name}
+                </Heading>
+                <IconButton
+                    size="2"
+                    variant="soft"
+                    color="red"
+                    onClick={() => removeIdea()}
+                >
+                    <TrashIcon />
+                </IconButton>
+            </Flex>
+            <hr />
 
-
-	return (
-		<Box
-			style={{
-				position: "absolute",
-				width: "400px",
-				maxHeight: "300px",
-				background: "#fff",
-				border: "2px solid #eaeefe",
-				top: "60%",
-				left: "60%",
-				padding: "28px",
-				borderRadius: "10px",
-				cursor: "text",
-				overflow: "scroll",
-				textAlign: "left",
-				resize: "both",
-			}}
-		>
-			<Flex justify='space-between'>
-				<Heading style={{ fontSize: "19px", fontWeight: "600" }}>
-					{idea.name}
-				</Heading>
-				<IconButton onClick={() => removeIdea()}>
-					<Cross1Icon />
-				</IconButton>
-			</Flex>
-			<hr />
-
-			<Editor workspace={workspace} name='idea' editorRef={editorRef} setUser={null} setCreated={setCreated} created={created}  saveChanges={saveChanges} data={idea.notes} />
-		</Box>
-	);
+            <Editor
+                workspace={workspace}
+                name="idea"
+                editorRef={editorRef}
+                setUser={null}
+                setCreated={setCreated}
+                created={created}
+                saveChanges={saveChanges}
+                data={idea.notes}
+            />
+        </Box>
+    );
 };
 
 export default IdeaInputBox;
--- components/Notes.tsx
+++ components/Notes.tsx
@@ -12,68 +12,81 @@ const Editor = dynamic(() => import("./Editor"), {
 });
 
 const Notes = ({ workspace }: { workspace: Workspace | undefined }) => {
-	const editorRef = useRef(null);
-	const [created, setCreated] = useState(false);
-	const [user, setUser] = useState<String | null>(null)
-	
-	const saveChanges = async () => {
-		if (editorRef.current && workspace) {
-			const outData: OutputData = await editorRef.current.save();
+    const editorRef = useRef(null);
+    const [created, setCreated] = useState(false);
+    const [user, setUser] = useState<String | null>(null);
 
-			// Searching a user with email
-			const noteIndex = workspace?.notes?.findIndex(
-				(note) => note.email === user
-			);
+    const saveChanges = async () => {
+        if (editorRef.current && workspace) {
+            // @ts-ignore
+            const outData: OutputData = await editorRef.current.save();
 
-			// Not found
-			if (noteIndex === -1 || noteIndex === undefined) {
-				await updateDoc(doc(db, "workspaces", workspace.id), {
-					notes: [
-						...(workspace.notes ? workspace.notes : []),
-						{ email: user, notes: outData },
-					],
-				});
-			} else {
-				// Found
-				if (workspace.notes) {
-					const updatedNotes = workspace.notes
-						? [...workspace?.notes]
-						: [];
-					updatedNotes[noteIndex] = {
-						email: user as string,
-						notes: outData,
-					};
+            // Searching a user with email
+            const noteIndex = workspace?.notes?.findIndex(
+                (note) => note.email === user
+            );
 
-					await updateDoc(doc(db, "workspaces", workspace.id), {
-						notes: updatedNotes,
-					});
-				}
-			}
-		}
-	};
+            // Not found
+            if (noteIndex === -1 || noteIndex === undefined) {
+                await updateDoc(doc(db, "workspaces", workspace.id), {
+                    notes: [
+                        ...(workspace.notes ? workspace.notes : []),
+                        { email: user, notes: outData },
+                    ],
+                });
+            } else {
+                // Found
+                if (workspace.notes) {
+                    const updatedNotes = workspace.notes
+                        ? [...workspace?.notes]
+                        : [];
+                    updatedNotes[noteIndex] = {
+                        email: user as string,
+                        notes: outData,
+                    };
 
-	return (
-		<Box
-			position="fixed"
-			top="0"
-			left="0"
-			height="100%"
-			px="6"
-			py="6"
-			style={{
-				width: "20%",
-				borderRight: "2px solid black",
-				overflowY: "scroll",
-				zIndex: 10,
-			}}
-		>
-			<Heading>Notes</Heading>
-			<hr />
+                    await updateDoc(doc(db, "workspaces", workspace.id), {
+                        notes: updatedNotes,
+                    });
+                }
+            }
+        }
+    };
 
-			<Editor editorRef={editorRef} name='note' saveChanges={saveChanges} setUser={setUser} workspace={workspace} setCreated={setCreated} created={created} data={workspace?.notes ? workspace?.notes?.find((n) => n.email === user)?.notes : undefined} />
+    return (
+        <Box
+            position="fixed"
+            top="0"
+            left="0"
+            height="100%"
+            px="6"
+            py="6"
+            style={{
+                width: "20%",
+                borderRight: "2px solid black",
+                overflowY: "scroll",
+                zIndex: 10,
+            }}
+        >
+            <Heading>Notes</Heading>
+            <hr />
 
-		</Box>
-	);
+            <Editor
+                editorRef={editorRef}
+                name="note"
+                saveChanges={saveChanges}
+                setUser={setUser}
+                workspace={workspace}
+                setCreated={setCreated}
+                created={created}
+                data={
+                    workspace?.notes
+                        ? workspace?.notes?.find((n) => n.email === user)?.notes
+                        : undefined
+                }
+            />
+        </Box>
+    );
 };
 
 export default Notes;
