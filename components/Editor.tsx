00aa0fc88b99680e865020d7b557469d2a057890
Vedanta Somnathe <vedanta1412@gmail.com>
2023-10-31 23:11:30 +0530

    set created

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -7,68 +7,69 @@ import { useSession } from "next-auth/react";
 
 interface Props {
     data: any;
-    editorRef:any;
-    saveChanges:any;
-    workspace:Workspace | undefined;
-    created:boolean;
-    setCreated:any;
-    setUser:any | null | undefined;
+    editorRef: any;
+    saveChanges: any;
+    workspace: Workspace | undefined;
+    created: boolean;
+    setCreated: any;
+    setUser: any | null | undefined;
     name: string;
 }
 
 export default function Editor({
-  data,
-  editorRef,
-  saveChanges,
-  workspace,
-  created,
-  setCreated,
-  setUser,
-  name
-}:Props) {
-    const holderRef = useRef(null)
-	const { data: session } = useSession();
+    data,
+    editorRef,
+    saveChanges,
+    workspace,
+    created,
+    setCreated,
+    setUser,
+    name,
+}: Props) {
+    const holderRef = useRef(null);
+    const { data: session } = useSession();
     const makeID = () => {
-		const newID = v4();
-		localStorage.setItem("temporary-user", newID);
-		return newID;
-	};
+        const newID = v4();
+        localStorage.setItem("temporary-user", newID);
+        return newID;
+    };
 
-  //initialize editorjs
-  useEffect(() => {
-    //initialize editor if we don't have a reference
-    if (holderRef.current && workspace&& !created) {
-        if(setUser) {
-            setUser(session && session.user
-                ? session?.user?.email
-                : localStorage.getItem("temporary-user")
-                ? localStorage.getItem("temporary-user")
-                : makeID())
-        }
-        console.log(name);
-        
-        editorRef.current = new EditorJS({
-          holder: holderRef.current,
-          tools: EDITOR_TOOLS,
-          data,
-          placeholder: "Write your heart out...",
-          onChange(api, event) {
-            saveChanges();
+    //initialize editorjs
+    useEffect(() => {
+        //initialize editor if we don't have a reference
+        if (holderRef.current && workspace && !created) {
+            if (setUser) {
+                setUser(
+                    session && session.user
+                        ? session?.user?.email
+                        : localStorage.getItem("temporary-user")
+                        ? localStorage.getItem("temporary-user")
+                        : makeID()
+                );
+            }
+            console.log(name);
+
+            editorRef.current = new EditorJS({
+                holder: holderRef.current,
+                tools: EDITOR_TOOLS,
+                data,
+                placeholder: "Write your heart out...",
+                onChange(api, event) {
+                    saveChanges();
+                },
+            });
+
             setCreated(true);
-          },
-        });
+        }
+    }, [holderRef, workspace]);
 
-    }
-
-  }, [holderRef, workspace]);
-
-  return (
-    <Box
-      style={{
-        paddingTop: "5px",
-      }}
-      id="notes"
-      ref={holderRef}
-    ></Box>
-  );
+    return (
+        <Box
+            style={{
+                paddingTop: "5px",
+            }}
+            id="notes"
+            ref={holderRef}
+        ></Box>
+    );
 }
